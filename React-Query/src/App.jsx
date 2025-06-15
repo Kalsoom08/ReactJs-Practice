0f9d2273bcb6ffc75de941fa97efc7f076c54151
Kalsoom08 <kalsoomyousafzai20@gmail.com>
2025-06-15 10:36:19 +0500

    Use Query Practice(Fetch data)

---
+++ React-Query/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Data from './Components/fetchData'
+
+const App = () => {
+  return (
+    <div>
+      <Data/>
+    </div>
+  )
+}
+
+export default App
---
+++ React-Query/src/Components/fetchData.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import axios from 'axios'
+import { useQuery } from '@tanstack/react-query';
+
+const FetchData = () => {
+   const {data , isLoading, error} = useQuery({
+    queryKey: 'fetch', 
+    queryFn : ()=> axios.get('https://67a0facf5bcfff4fabe147df.mockapi.io/ecommercewebsite').then(
+        res => res.data
+    )
+   })
+
+    if(isLoading) return <p>Loading....</p>
+    if(error) return <p>{error.message}</p>
+  return (
+    <ul>
+        {
+            data.map(u=>(
+                <li key={u.id}>
+                    <li>Title : {u.title}</li>
+                    
+                </li>
+            ))
+        }
+    </ul>
+  )
+}
+
+export default FetchData
